f415d883ddcd238bcc2e562224b75c02f3c920af
Ishan <ishan.a.shah3.14@gmail.com>
2024-08-27 09:58:18 -0500

    make benchark more accurate

--- benchmarks/concurrent_queue.c
+++ benchmarks/concurrent_queue.c
@@ -19,22 +19,22 @@ struct test_config {
 struct thread_args {
     struct llcm_concurrent_queue *queue;
     struct test_config const *config;
-    uint64_t *push_counter;
     uint64_t *start_barrier;
+    uint64_t *end_barrier;
 };
 
 void *thread_exec(void *arg0) {
     struct thread_args *thread_args = arg0;
     struct llcm_concurrent_queue *queue = thread_args->queue;
-    uint64_t *push_counter = thread_args->push_counter;
-    uint64_t tid = __atomic_fetch_add(thread_args->start_barrier, 1, __ATOMIC_SEQ_CST);
+    uint64_t *start_barrier = thread_args->start_barrier;
+    uint64_t *end_barrier = thread_args->end_barrier;
+    uint64_t tid = __atomic_fetch_add(start_barrier, 1, __ATOMIC_SEQ_CST);
     thread_perf_mode_init(tid);
-    while (__atomic_load_n(thread_args->start_barrier, __ATOMIC_SEQ_CST) !=
+    while (__atomic_load_n(start_barrier, __ATOMIC_SEQ_CST) !=
            thread_args->config->num_threads + 1) {
     }
 
-    for (; __atomic_load_n(push_counter, __ATOMIC_SEQ_CST) < MAX_SEQUENCE;
-         __atomic_fetch_add(push_counter, 1, __ATOMIC_SEQ_CST)) {
+    while (__atomic_load_n(end_barrier, __ATOMIC_SEQ_CST) == 0) {
         void *pop_result = NULL;
         while (NULL == pop_result) {
             pop_result = llcm_concurrent_queue_try_pop(queue);
@@ -47,18 +47,20 @@ void *thread_exec(void *arg0) {
 
 uint64_t multithreaded_test(struct test_config config) {
     struct llcm_concurrent_queue queue;
-    alignas(LLCM_CONCURRENT_QUEUE_CACHE_LINE_SIZE) uint64_t push_counter = 0;
     alignas(LLCM_CONCURRENT_QUEUE_CACHE_LINE_SIZE) uint64_t start_barrier = 0;
+    alignas(LLCM_CONCURRENT_QUEUE_CACHE_LINE_SIZE) uint64_t end_barrier = 0;
     llcm_concurrent_queue_init(&queue, config.num_elements);
     llcm_concurrent_queue_try_reserve_size_before_push(&queue, config.num_elements);
     for (size_t i = 0; i < config.num_elements; i++) {
         llcm_concurrent_queue_push(&queue, DUMMY_ELEMENT);
     }
 
-    struct thread_args thread_args = {.queue = &queue,
-                                      .config = &config,
-                                      .push_counter = &push_counter,
-                                      .start_barrier = &start_barrier};
+    struct thread_args thread_args = {
+        .queue = &queue,
+        .config = &config,
+        .start_barrier = &start_barrier,
+        .end_barrier = &end_barrier,
+    };
     pthread_t threads[config.num_threads];
     for (size_t tid = 0; tid < config.num_threads; tid++) {
         int rc = pthread_create(&threads[tid], NULL, thread_exec, &thread_args);
@@ -72,9 +74,10 @@ uint64_t multithreaded_test(struct test_config config) {
     }
 
     uint64_t const start_time = rdtsc();
-    while (__atomic_load_n(&push_counter, __ATOMIC_SEQ_CST) < MAX_SEQUENCE) {
+    while (__atomic_load_n(&queue.write_counter, __ATOMIC_SEQ_CST) < MAX_SEQUENCE) {
     }
     uint64_t const end_time = rdtsc();
+    __atomic_store_n(&end_barrier, 1, __ATOMIC_SEQ_CST);
 
     for (size_t tid = 0; tid < config.num_threads; tid++) {
         pthread_join(threads[tid], NULL);
@@ -101,11 +104,8 @@ void aggregate_test(struct test_config config) {
 
 int main() {
     aggregate_test((struct test_config){.num_elements = 1, .num_threads = 1});
-    // aggregate_test((struct test_config){.num_elements = 1, .num_threads = 2});
-    // aggregate_test((struct test_config){.num_elements = 1, .num_threads = 4});
     aggregate_test((struct test_config){.num_elements = 2, .num_threads = 1});
     aggregate_test((struct test_config){.num_elements = 2, .num_threads = 2});
-    // aggregate_test((struct test_config){.num_elements = 2, .num_threads = 4});
     aggregate_test((struct test_config){.num_elements = 4, .num_threads = 1});
     aggregate_test((struct test_config){.num_elements = 4, .num_threads = 2});
     aggregate_test((struct test_config){.num_elements = 4, .num_threads = 4});
