4566d17c8610ced7f25334c5f66a924b1f12fdcd
Ishan <ishan.a.shah3.14@gmail.com>
2025-04-09 17:52:44 -0700

    make cpp version of concurrent queue

---
+++ cpp/lib/concurrent_queue.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include <optional>
+#include <stddef.h>
+#include <stdint.h>
+#include <vector>
+
+template <typename T> class ConcurrentQueue {
+  public:
+    ConcurrentQueue(size_t capacity);
+
+    size_t GetCapacity() const { return mask_ + 1; }
+
+    bool TryReserveSizeBeforePush(size_t num_new_entries);
+    void UnreserveSizeAfterPop(size_t num_old_entries);
+    void Push(T);
+    std::optional<T> TryPop();
+
+  private:
+    static constexpr size_t CACHE_LINE_SIZE = 64;
+    struct Entry {
+        alignas(CACHE_LINE_SIZE) volatile uint64_t aba_counter_ = 0;
+        T element_;
+    };
+    static_assert(alignof(Entry) >= CACHE_LINE_SIZE, "");
+    static constexpr uint64_t llcm_round_up_pow2(uint64_t x) {
+        return x == 1 ? 1 : 1 << (64 - __builtin_clzl(x - 1));
+    }
+
+  private:
+    std::vector<Entry> array_;
+    size_t mask_ = 0;
+
+    alignas(CACHE_LINE_SIZE) uint64_t read_counter_ = 0;
+    alignas(CACHE_LINE_SIZE) uint64_t write_counter_ = 0;
+    alignas(CACHE_LINE_SIZE) uint64_t reserved_push_size_ = 0;
+};
+
+template <typename T> ConcurrentQueue<T>::ConcurrentQueue(size_t capacity) {
+    capacity = llcm_round_up_pow2(capacity);
+    if (capacity < 2) {
+        capacity = 2;   // capacity must be at least 2 for aba_counter
+    }
+    array_ = std::vector<Entry>(capacity);
+    for (uint64_t i = 0; i < capacity; i++) {
+        array_[i].aba_counter_ = i;
+    }
+    mask_ = capacity - 1;
+}
+
+template <typename T> bool ConcurrentQueue<T>::TryReserveSizeBeforePush(size_t num_new_entries) {
+    uint64_t const reserved_push_size =
+        __atomic_fetch_add(&reserved_push_size_, num_new_entries, __ATOMIC_SEQ_CST);
+    if (reserved_push_size > mask_) {
+        __atomic_fetch_sub(&reserved_push_size_, num_new_entries, __ATOMIC_SEQ_CST);
+        return false;
+    }
+    return true;
+}
+
+template <typename T> void ConcurrentQueue<T>::UnreserveSizeAfterPop(size_t num_old_entries) {
+    __atomic_fetch_sub(&reserved_push_size_, num_old_entries, __ATOMIC_SEQ_CST);
+}
+
+template <typename T> void ConcurrentQueue<T>::Push(T value) {
+    uint64_t const reserved_write_counter =
+        __atomic_fetch_add(&write_counter_, 1, __ATOMIC_SEQ_CST);
+    struct Entry *entry = &array_[reserved_write_counter & mask_];
+    while (entry->aba_counter_ != reserved_write_counter) {
+    }
+    entry->element_ = value;
+    __asm__ __volatile__("" ::: "memory");
+    entry->aba_counter_ = reserved_write_counter + 1;
+}
+
+template <typename T> std::optional<T> ConcurrentQueue<T>::TryPop() {
+    uint64_t const local_write_counter = write_counter_;
+    uint64_t *read_ptr = &read_counter_;
+    uint64_t local_read_counter = *read_ptr;
+    while (local_read_counter < local_write_counter) {
+        if (__atomic_compare_exchange_n(read_ptr, &local_read_counter, local_read_counter + 1,
+                                        false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)) {
+            struct Entry *entry = &array_[local_read_counter & mask_];
+            while (entry->aba_counter_ != local_read_counter + 1) {
+            }
+            void *read_value = entry->element_;
+            __asm__ __volatile__("" ::: "memory");
+            entry->aba_counter_ = local_read_counter + mask_ + 1;
+            return read_value;
+        }
+    }
+    return std::nullopt;
+}
---
+++ cpp/tests/concurrent_queue_test.c
@@ -0,0 +1,116 @@
+#include "lib/concurrent_queue.h"
+
+#include <cassert>
+#include <optional>
+#include <pthread.h>
+#include <stdio.h>
+
+void basic_queue_test() {
+    size_t const capacity = 16;
+    ConcurrentQueue<void *> queue(capacity);
+    assert(capacity == queue.GetCapacity());
+
+    for (uint64_t i = 1; i <= capacity * 4; i++) {
+        bool const reserve_result = queue.TryReserveSizeBeforePush(1);
+        assert(reserve_result);
+        queue.Push((void *) i);
+        void *pop_result = queue.TryPop().value();
+        assert((uint64_t) pop_result == i);
+        queue.UnreserveSizeAfterPop(1);
+    }
+
+    // reserve capacity entries to push
+    for (uint64_t i = 1; i <= capacity; i++) {
+        bool const reserve_result = queue.TryReserveSizeBeforePush(1);
+        assert(reserve_result);
+    }
+
+    // reserving capacity + 1th entry should fail
+    {
+        bool const reserve_result = queue.TryReserveSizeBeforePush(1);
+        assert(!reserve_result);
+    }
+
+    // reserve capacity entries at once
+    {
+        queue.UnreserveSizeAfterPop(capacity);
+        bool const reserve_result = queue.TryReserveSizeBeforePush(capacity);
+        assert(reserve_result);
+    }
+
+    for (uint64_t i = 1; i <= capacity; i++) {
+        queue.Push((void *) i);
+    }
+    for (uint64_t i = 1; i <= capacity; i++) {
+        void *pop_result = queue.TryPop().value();
+        assert((uint64_t) pop_result == i);
+    }
+    assert(std::nullopt == queue.TryPop());
+    queue.UnreserveSizeAfterPop(capacity);
+
+    printf("PASSED basic_queue_test\n");
+}
+
+#define MULTITHREADED_TEST_NUM_THREADS  32
+#define MULTITHREADED_TEST_MAX_SEQUENCE 1024
+
+class SequenceTracker {
+  public:
+    void MarkObserved(uint64_t sequence_number) {
+        __atomic_fetch_add(&num_observations_[sequence_number], 1, __ATOMIC_SEQ_CST);
+    }
+    void Validate() {
+        for (uint64_t sequence = 1; sequence < MULTITHREADED_TEST_MAX_SEQUENCE; sequence++) {
+            assert(num_observations_[sequence] == MULTITHREADED_TEST_NUM_THREADS);
+        }
+    }
+
+  private:
+    uint64_t num_observations_[MULTITHREADED_TEST_MAX_SEQUENCE] = {};
+};
+
+struct thread_args {
+    ConcurrentQueue<void *> *queue_ = nullptr;
+    SequenceTracker *sequence_tracker_ = nullptr;
+};
+
+void *thread_exec(void *arg0) {
+    struct thread_args *args = reinterpret_cast<struct thread_args *>(arg0);
+    for (uint64_t sequence_number = 1; sequence_number < MULTITHREADED_TEST_MAX_SEQUENCE;
+         sequence_number++) {
+        args->queue_->Push((void *) sequence_number);
+        std::optional<void *> pop_result = std::nullopt;
+        while (std::nullopt == pop_result) {
+            pop_result = args->queue_->TryPop();
+        }
+        args->sequence_tracker_->MarkObserved((uint64_t) pop_result.value());
+    }
+    return NULL;
+}
+
+void multithreaded_test() {
+    ConcurrentQueue<void *> queue(MULTITHREADED_TEST_NUM_THREADS);
+    bool const was_reserved = queue.TryReserveSizeBeforePush(MULTITHREADED_TEST_NUM_THREADS);
+    assert(was_reserved);
+    SequenceTracker sequence_tracker;
+
+    struct thread_args thread_args = {.queue_ = &queue, .sequence_tracker_ = &sequence_tracker};
+    pthread_t threads[MULTITHREADED_TEST_NUM_THREADS];
+    for (size_t tid = 0; tid < MULTITHREADED_TEST_NUM_THREADS; tid++) {
+        int rc = pthread_create(&threads[tid], NULL, thread_exec, &thread_args);
+        assert(rc == 0);
+    }
+
+    for (size_t tid = 0; tid < MULTITHREADED_TEST_NUM_THREADS; tid++) {
+        pthread_join(threads[tid], NULL);
+    }
+
+    queue.UnreserveSizeAfterPop(MULTITHREADED_TEST_NUM_THREADS);
+    sequence_tracker.Validate();
+    printf("PASSED multithreaded_test\n");
+}
+
+int main() {
+    basic_queue_test();
+    multithreaded_test();
+}
